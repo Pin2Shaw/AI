69489c374f907c5b60670988d654d6942100941c
Pin2Shaw <36593803+Pin2Shaw@users.noreply.github.com>
2018-03-26 13:54:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-from cloudant import Cloudant 
-
-user= "9ef80d52-30b9-4e45-833a-75059db0825c-bluemix"
-password= "79d7c792bb13072da32aecc7dc3777e28780d6d11619795d54d717d2abbd62e5"
-host= "9ef80d52-30b9-4e45-833a-75059db0825c-bluemix.cloudant.com"
-url = 'https://' + host
-client = Cloudant(user, password, url=url, connect=True)   
 
 print("*****************")
 print ("Gunalan")
