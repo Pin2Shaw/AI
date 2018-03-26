4d7fe9cc72e24929290f8ef9daa21f529302eab1
Pin2Shaw <36593803+Pin2Shaw@users.noreply.github.com>
2018-03-26 13:52:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from cloudant import cloudant 
+from cloudant import Cloudant 
 
 user= "9ef80d52-30b9-4e45-833a-75059db0825c-bluemix"
 password= "79d7c792bb13072da32aecc7dc3777e28780d6d11619795d54d717d2abbd62e5"
