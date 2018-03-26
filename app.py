89e9a362b629771105cc993a6ed8fd0c2cd9f3fe
Pin2Shaw <36593803+Pin2Shaw@users.noreply.github.com>
2018-03-26 13:39:18 +0530

    app.py
    ReadCloudant

---
+++ app.py
@@ -0,0 +1,17 @@
+from cloudant import cloudant 
+
+user= "9ef80d52-30b9-4e45-833a-75059db0825c-bluemix"
+password= "79d7c792bb13072da32aecc7dc3777e28780d6d11619795d54d717d2abbd62e5"
+host= "9ef80d52-30b9-4e45-833a-75059db0825c-bluemix.cloudant.com"
+url = 'https://' + host
+client = Cloudant(user, password, url=url, connect=True)   
+
+app = Flask(__name__)
+app.config['SECRET_KEY']="QWERTYUIOPASDFGHJKLZXCVBNM"
+
+
+@app.route('/')
+
+Heroku
+Requirement.txt
+proc file
